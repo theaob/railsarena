b791fd60a43f00e4c2bd9b8b1f15eb2231de4bcd
Onur Baykal <theaob@gmail.com>
2012-02-23 21:48:47 +0200

    Rails Files Are Added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsarena::Application.config.secret_token = '999cd25e82cd457b808177fa0b9206d2e5b783de2da142940f43f8d8b936ee204c22fa7167f56a5c9445a5c6dc494c86362178dca8972993b85b6f8c80d5e2ab'
